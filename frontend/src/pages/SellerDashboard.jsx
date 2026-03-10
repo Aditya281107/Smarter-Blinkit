630885e0e7596cb7bf1ba3be4025e42fe31c3bc1
Aditya281107 <69746212+Aditya281107@users.noreply.github.com>
2026-03-10 12:18:51 +0530

    Add files via upload

---
+++ frontend/src/pages/SellerDashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SellerDashboard(){
+
+ return(
+  <div>
+
+   <h2>Seller Dashboard</h2>
+
+   <p>Add products and manage inventory</p>
+
+  </div>
+ )
+}
+
+export default SellerDashboard;
